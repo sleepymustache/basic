1673a663ec7ea2f20f95499ea892786b52210df8
Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
2013-06-12 14:48:18 -0700

    Fixed typo

--- modules/urlclass.php
+++ modules/urlclass.php
@@ -1,17 +1,21 @@
 <?php
 	function wizard_render_placeholder_urlClass() {
+		// Get the current URL
 		$url = $_SERVER['REQUEST_URI'];
 
+		// Remove the parameters
 		if ($parameters = strlen($url) - (strlen($url) - strpos($url, "?"))) {
 			$url = substr($url, 0, $parameters);
 		}
 
+		// If it doesn't end in php, then add default page
 		if (!strpos($url, ".php")) {
 			$url = $url . "index";
 		} else {
-			$url = strstr($url, 0, strlen($url) - 4);
+			$url = substr($url, 0, strlen($url) - 4);
 		}
 
+		// Remove trailing slash
 		if (strpos($url, "/") == 0) {
 			$url = substr($url, 1, strlen($url) - 1);
 		}
@@ -21,7 +25,7 @@
 		$url = str_replace($prefix, "", str_replace("/", "-", $url));
 
 		if (empty($url)) {
-			$url = 'index';
+			$url = 'index2';
 		}
 
 		return $url;
