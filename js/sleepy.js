c19545b15954dd514331f773a88aa083ded42978
Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
2013-10-10 14:24:34 -0700

    Add cookie functionality

--- js/sleepy.js
+++ js/sleepy.js
@@ -68,6 +68,32 @@ define([], function () {
 			for (index in files) {
 				appendStylesheet(files[index]);
 			}
-		}
+		},
+
+		// sets a cookie
+		'setCookie': function (c_name, value, exdays) {
+			var exdate,
+				c_value;
+			exdate = new Date();
+			exdate.setDate(exdate.getDate() + exdays);
+			c_value = escape(value) + ((exdays === null) ? "" : "; expires=" + exdate.toUTCString());
+			document.cookie = c_name + "=" + c_value + "; path=/";
+		},
+
+		// reads a cookie
+		'getCookie': function (c_name) {
+			var i,
+				x,
+				y,
+				ARRcookies = document.cookie.split(";");
+			for (i = 0;i < ARRcookies.length; i++) {
+				x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+				y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+				x = x.replace(/^\s+|\s+$/g, "");
+				if (x === c_name) {
+					return unescape(y);
+				}
+			}
+		},
 	};
 });
