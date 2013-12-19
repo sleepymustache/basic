224f8a9ccf74f22491bc19a41263aec9a1416cc8
fr-atom <joseph.bomber@gmail.com>
2013-12-19 14:38:26 -0800

    updated head-inserter and robots-dev-hide
    header-inserter: now checks to see if there is no head. If none is
    found, it does not insert. also added comments
    
    robots-dev-hide: added coments

--- modules/enabled/header-inserter/insterter.php
+++ modules/enabled/header-inserter/insterter.php
@@ -1,10 +1,19 @@
 <?php
+/*
+	creates a new Hook called head_inserter, when
+	called this hook inserts the given string just above
+	the closing head tag. If </head> is not found, 
+	nothing is inserted
+*/
 
 function insertHeadStuffJF ($html) {
 	$toInsert = Hook::addFilter('head_inserter', '');
 
-	$position = strrpos($html, '</head>');
-	$html = substr_replace($html, $toInsert, $position, 0);
+	$pos = strrpos($html, '</head>');
+
+	$html = ($pos !== -1) 
+			? substr_replace($html, $toInsert, $pos, 0) 
+			: $html;
 
 	return $html;
 }
--- modules/enabled/robots-dev-hide/hider.php
+++ modules/enabled/robots-dev-hide/hider.php
@@ -1,5 +1,9 @@
 <?php
 
+/*
+	adds do not follow tag to head of document
+	this module depends on the head_insert module
+*/
 function robotsDevHide () {
 	$unfollow = '<META NAME="ROBOTS" CONTENT="NOINDEX, NOFOLLOW">';
 
