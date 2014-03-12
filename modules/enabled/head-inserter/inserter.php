91dc99bcfdac8c3deb3f9f01ed12ded54e0e8100
Jaime Rodriguez <jaime@bedroom.(none)>
2014-03-11 17:27:54 -0700

    Formatting issues

--- modules/enabled/head-inserter/inserter.php
+++ modules/enabled/head-inserter/inserter.php
@@ -1,16 +1,13 @@
 <?php
 /*
-	creates a new Hook called head_inserter, when
+	Creates a new Hook called head_inserter, when
 	called this hook inserts the given string just above
 	the closing head tag. If </head> is not found, 
 	nothing is inserted
 */
-
 function insertHeadStuffJF ($html) {
 	$toInsert = Hook::addFilter('head_inserter', "\n");
-
 	$pos = strrpos($html, '</head>');
-
 	$html = ($pos !== false) 
 			? substr_replace($html, $toInsert, $pos, 0) 
 			: $html;
