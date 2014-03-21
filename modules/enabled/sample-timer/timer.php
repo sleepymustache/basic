64024ca76a7aa4ec34a980b9f546c2f68da1c0e7
Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
2014-03-20 17:40:21 -0700

    Added memory information to the timer... kinda jenky but I want to know

--- modules/enabled/sample-timer/timer.php
+++ modules/enabled/sample-timer/timer.php
@@ -1,6 +1,6 @@
 <?php
 function timer_render_placeholder() {
-	return Performance::stop('template');
+	return Performance::stop('template') . " Mem: " . memory_get_peak_usage(true);
 }
 
 Hook::applyFilter('render_placeholder_timer', 'timer_render_placeholder');
