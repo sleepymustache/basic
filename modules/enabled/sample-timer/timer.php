4275a94ae96f4c0a2ab3374a032db42c1203f603
jaimerod <hi.i.am.jaime@gmail.com>
2014-08-13 19:11:24 -0700

    Moved to disabled by default

--- modules/enabled/sample-timer/timer.php
+++
@@ -1,6 +0,0 @@
-<?php
-function timer_render_placeholder() {
-	return Performance::stop('template') . " Mem: " . memory_get_peak_usage(true);
-}
-
-Hook::applyFilter('render_placeholder_timer', 'timer_render_placeholder');
