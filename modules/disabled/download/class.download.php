b31b972d3dd289d8b98cc93b6b3c1b9b865d9c4a
jaimerod <hi.i.am.jaime@gmail.com>
2014-08-13 19:01:40 -0700

    Added namespace

--- modules/disabled/download/class.download.php
+++ modules/disabled/download/class.download.php
@@ -1,13 +1,37 @@
 <?php
-	class ForceDownload {
-		public function getAnchor($text, $link, $target="_blank") {
-			return "<a href=\"" . URLBASE . "modules/enabled/download/?url={$link}\">{$text}</a>";
-		}
+namespace Download;
 
-		public function download($file) {
-			header('Content-Type: application/octet-stream');
-			header("Content-Transfer-Encoding: Binary"); 
-			header("Content-disposition: attachment; filename=\"" . basename($file) . "\""); 
-			readfile($file);	
-		}
+/**
+ * @page download Download Class
+ *
+ * Used to force a download of a file
+ *
+ * @section usage Usage
+ * @code
+ *   $d= new Download/Forces()
+ *
+ *   $d->getAnchor('ISI', 'isi.pdf');
+ * @endcode
+ *
+ * @section changelog Changelog
+ *   ## Version 1.2
+ *   * Added namespacing
+ *
+ * @date August 13, 2014
+ * @author Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
+ * @version 1.2
+ * @copyright  GPL 3 http://rodriguez-jr.com
+ */
+
+class Forces {
+	public function getAnchor($text, $link, $target="_blank") {
+		return "<a target=\"{$target}\" href=\"" . URLBASE . "modules/enabled/download/?url={$link}\">{$text}</a>";
 	}
+
+	public function download($file) {
+		header('Content-Type: application/octet-stream');
+		header("Content-Transfer-Encoding: Binary");
+		header("Content-disposition: attachment; filename=\"" . basename($file) . "\"");
+		readfile($file);
+	}
+}
--- modules/disabled/download/index.php
+++ modules/disabled/download/index.php
@@ -1,10 +1,8 @@
 <?php
-	require_once(dirname(__FILE__) . '/../../../include/class.debug.php');
-	require_once(dirname(__FILE__) . '/../../../include/global.php');
-	require_once('class.download.php');
-	
-	if (isset($_GET['url'])) {
-		$fd = new ForceDownload();
-		$fd->download(DIRBASE . $_GET['url']);
-	}
-?>
+require_once(dirname(__FILE__) . '/../../../include/global.php');
+require_once('class.download.php');
+
+if (isset($_GET['url'])) {
+	$fd = new \Download\Force();
+	$fd->download(DIRBASE . $_GET['url']);
+}
