127b45fe303287c77e2c7ad42db29b1ee0b73a2b
jaimerod <hi.i.am.jaime@gmail.com>
2014-08-13 18:42:38 -0700

    Removed the performance class Added a check for the setup

--- include/sleepy.php
+++ include/sleepy.php
@@ -1,27 +1,27 @@
 <?php
-	// Performance benchmarking
-	require_once('class.performance.php');
-	Performance::start('template');
+// Enable sessions
+session_start();
 
-	// Enable sessions
-	session_start();
+// If we are not setup yet, forward the user to the setup page
+if (!@include_once('global.php')) {
+	header('Location: /setup/');
+	die();
+}
 
-	include_once('class.debug.php');
-	include_once('global.php');
-	include_once('class.hooks.php');
-	include_once('class.template.php');
+require_once('class.hooks.php');
+require_once('class.template.php');
 
-	class Sleepy {
-		public function __construct() {
-			Hook::addAction('sleepy_preprocess');
+class Sleepy {
+	public function __construct() {
+		\Sleepy\Hook::addAction('sleepy_preprocess');
 
-			// Send the encoding ahead of time to speed up rendering
-			header('Content-Type: text/html; charset=utf-8');
-		}
-
-		public function __destruct() {
-			Hook::addAction('sleepy_postprocess');
-		}
+		// Send the encoding ahead of time to speed up rendering
+		header('Content-Type: text/html; charset=utf-8');
 	}
 
-	$_sleepy = new Sleepy();
+	public function __destruct() {
+		\Sleepy\Hook::addAction('sleepy_postprocess');
+	}
+}
+
+$_sleepy = new Sleepy();
