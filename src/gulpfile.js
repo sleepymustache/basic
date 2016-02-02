41ff37449ca4a0129e24d697e22572fb2d6c0239
Jaime Rodriguez <jrodriguez@giantagency.com>
2016-02-01 16:53:18 -0800

    Updated gulp packages and restructures SCSS slightly

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,35 +2,82 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var livereload = require('gulp-livereload');
+var notify = require('gulp-notify');
 var sass = require('gulp-ruby-sass');
+var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 // Source Folders
 var imageFolder = 'images';
 var jsFolder = 'js';
 var mainSassFile = 'main.scss';
-var sassFolder = 'scss'
+var sassFolder = 'scss';
 
 // Build Folders
 var buildCssFolder = 'build/css';
 var buildImageFolder = 'build/img';
 var buildJsFolder = 'build/js';
 
+function handleErrors() {
+  console.log('handling error');
+  var args = Array.prototype.slice.call(arguments);
+  notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+  this.emit('end'); // Keep gulp from hanging on this task
+}
+
+function buildScript(file, watch) {
+  var props = {
+    entries: ['./' + jsFolder + '/' + file],
+    debug : true,
+    transform:  [reactify]
+  };
+
+  // watchify() if watch requested, otherwise run browserify() once
+  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream
+      .on('error', handleErrors)
+      .pipe(source(file))
+      .pipe(buffer())
+      //.pipe(uglify())
+      .pipe(gulp.dest('./' + buildJsFolder + '/'))
+      .pipe(livereload());
+  }
+
+  // listen for an update and run rebundle
+  bundler.on('update', function() {
+    rebundle();
+    console.log('Rebundle...');
+  });
+
+  // run it once the first time buildScript is called
+  return rebundle();
+}
+
 /**
  * Compiles SCSS to CSS and minifies CSS
  */
 gulp.task('styles', function () {
-  var sassOptions = {
-    'sourcemapPath': '../' + sassFolder,
+ var sassOptions = {
+    'sourcemap': true,
     'style': 'compressed'
   };
 
-  return gulp.src(sassFolder + '/' + mainSassFile)
-    .pipe(sass(sassOptions).
-      on('error', function (err) {
+  return sass('scss/**/*.scss', sassOptions)
+    .on('error', function (err) {
         console.error("Error", err.message);
-      }))
-    .pipe(gulp.dest(buildCssFolder))
+    })
+    .pipe(sourcemaps.init({debug: true}))
+    .pipe(sourcemaps.write('./', {
+      includeContent: true,
+      sourceRoot: './'
+    }))
+    .pipe(gulp.dest('./' + buildCssFolder))
     .pipe(livereload());
 });
 
@@ -50,7 +97,9 @@ gulp.task('watch', function () {
 gulp.task('scripts', function () {
   gulp.src(jsFolder + '/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest(buildJsFolder));
+    .on('error', handleErrors)
+    .pipe(gulp.dest(buildJsFolder))
+    .pipe(livereload());
 });
 
 /**
@@ -58,6 +107,7 @@ gulp.task('scripts', function () {
  */
 gulp.task('images', function () {
   gulp.src(imageFolder + '/*')
+    .on('error', handleErrors)
     .pipe(imagemin())
     .pipe(gulp.dest(buildImageFolder));
 })
