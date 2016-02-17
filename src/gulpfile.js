1f38e8f2efee937b24a60ab34e98bbd57f9c83fd
Jaime Rodriguez <jrodriguez@giantagency.com>
2016-02-16 16:36:32 -0800

    refactor: reorder code for sense

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -19,45 +19,41 @@ var buildImageFolder = 'build/img';
 var buildJsFolder = 'build/js';
 
 function handleErrors() {
-  console.log('handling error');
   var args = Array.prototype.slice.call(arguments);
+
   notify.onError({
     title: 'Compile Error',
     message: '<%= error.message %>'
   }).apply(this, args);
-  this.emit('end'); // Keep gulp from hanging on this task
+
+  this.emit('end');
 }
 
-function buildScript(file, watch) {
-  var props = {
-    entries: ['./' + jsFolder + '/' + file],
-    debug : true,
-    transform:  [reactify]
-  };
+/**
+ * Runs by default
+ */
+gulp.task('default', ['images', 'scripts', 'styles', 'watch'], function () {});
 
-  // watchify() if watch requested, otherwise run browserify() once
-  var bundler = watch ? watchify(browserify(props)) : browserify(props);
+/**
+ * Compresses image files for production
+ */
+gulp.task('images', function () {
+  gulp.src(imageFolder + '/*')
+    .on('error', handleErrors)
+    .pipe(imagemin())
+    .pipe(gulp.dest(buildImageFolder));
+});
 
-  function rebundle() {
-    var stream = bundler.bundle();
-    return stream
-      .on('error', handleErrors)
-      .pipe(source(file))
-      .pipe(buffer())
-      //.pipe(uglify())
-      .pipe(gulp.dest('./' + buildJsFolder + '/'))
-      .pipe(livereload());
-  }
-
-  // listen for an update and run rebundle
-  bundler.on('update', function() {
-    rebundle();
-    console.log('Rebundle...');
-  });
-
-  // run it once the first time buildScript is called
-  return rebundle();
-}
+/**
+ * Minifies JS files for production
+ */
+gulp.task('scripts', function () {
+  gulp.src(jsFolder + '/*.js')
+    .pipe(uglify())
+    .on('error', handleErrors)
+    .pipe(gulp.dest(buildJsFolder))
+    .pipe(livereload());
+});
 
 /**
  * Compiles SCSS to CSS and minifies CSS
@@ -69,9 +65,7 @@ gulp.task('styles', function () {
   };
 
   return sass('scss/**/*.scss', sassOptions)
-    .on('error', function (err) {
-        console.error("Error", err.message);
-    })
+    .on('error', handleErrors)
     .pipe(sourcemaps.init({debug: true}))
     .pipe(sourcemaps.write('./', {
       includeContent: true,
@@ -90,29 +84,3 @@ gulp.task('watch', function () {
   gulp.watch([sassFolder + '/**/*.scss'], ['styles']);
   gulp.watch([imageFolder + '/**/*'], ['images']);
 });
-
-/**
- * Minifies JS files for production
- */
-gulp.task('scripts', function () {
-  gulp.src(jsFolder + '/*.js')
-    .pipe(uglify())
-    .on('error', handleErrors)
-    .pipe(gulp.dest(buildJsFolder))
-    .pipe(livereload());
-});
-
-/**
- * Compresses image files for production
- */
-gulp.task('images', function () {
-  gulp.src(imageFolder + '/*')
-    .on('error', handleErrors)
-    .pipe(imagemin())
-    .pipe(gulp.dest(buildImageFolder));
-})
-
-/**
- * Runs by default
- */
-gulp.task('default', ['images', 'scripts', 'styles', 'watch'], function () {});
