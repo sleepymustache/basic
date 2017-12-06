cae44d6e77b3c78eee962ac212d6348b4e6341ce
akrikorian85 <akrikorian85@gmail.com>
2017-12-06 15:26:41 -0800

    Update: added teamsite detection for document root

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-	require_once($_SERVER['DOCUMENT_ROOT'] . '/app/core/sleepy.php');
+	require_once(((@include_once('Webkit/init.php')) ? $docRoot : $_SERVER['DOCUMENT_ROOT']) . '/app/core/sleepy.php');
 
 	$page = new \Sleepy\Template('homepage');
 
