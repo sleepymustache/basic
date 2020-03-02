f0067914cbdb6816892de91c87b43edcccb9daca
Jaime Rodriguez <hi.i.am.jaime@gmail.com>
2020-03-02 14:39:05 -0800

    update: PSR-12 styleguide

--- src/index.php
+++ src/index.php
@@ -1,53 +1,78 @@
 <?php
-  require_once(((@include_once('Webkit/init.php')) ? $docRoot : $_SERVER['DOCUMENT_ROOT']) . '/app/core/sleepy.php');
+/**
+ * Index Page
+ *
+ * PHP version 7.0.0
+ *
+ * @category Page
+ * @package  Sleepy
+ * @author   Jaime A. Rodriguez <hi.i.am.jaime@gmail.com>
+ * @license  http://opensource.org/licenses/MIT; MIT
+ * @link     https://sleepymustache.com
+ */
 
-  use \Sleepy\Template;
+require_once $_SERVER['DOCUMENT_ROOT'] . '/app/sleepy/bootstrap.php';
 
-  $page = new Template('homepage');
+use \Sleepy\Core\Template;
+use \Sleepy\Core\Debug;
+use \Sleepy\Core\Hook;
 
-  // SEO
-  $page->bind([
-    'title'       => 'Sleepy Mustache',
-    'description' => 'This is the description',
-    'keywords'    => "log, sleepy mustache, framework"
-  ]);
+$page = new Template('homepage');
 
-  // Content
-  $page->bind('header', 'sleepy<span>MUSTACHE</span>');
-  $page->bind('teasers', [
+// SEO
+$page->bind(
     [
-      "title"   => "Getting Started",
-      "link"    => 'http://www.sleepymustache.com/',
-      "author"  => "Jaime A. Rodriguez",
-      "date"    => date('m/d/Y', time()),
-      "description" => "
-        Congratulations on successfully installing sleepyMUSTACHE! You can visit the <a href=\"http://www.sleepymustache.com/documentation/index.html\">documentation page</a> to learn more or hit the ground running by viewing the <a href=\"http://www.sleepymustache.com/#getting-started\">getting started</a> section.",
-      "tags"    => [
-        [
-          'name' => "Configuration",
-          'link' => "http://www.sleepymustache.com/#getting-started"
-        ]
-      ]
-    ], [
-      "title"   => "Sample Modules",
-      "link"    => "#",
-      "author"  => "Jaime A. Rodriguez",
-      "date"    => date('m/d/Y', time() - 30 * 24 * 60 * 60),
-      "description" => "
-        By default there are 2 sample modules included with the
-        framework. These modules demonstrate how to create your own
-        modules, and implement existing functionality. You may safely
-        delete them.",
-      "tags"    => [
-        [
-          'name' => "modules",
-          'link' => "http://www.sleepymustache.com/#default-modules"
-        ], [
-          'name' => "fixes",
-          'link' => "https://github.com/jaimerod/sleepy-mustache/commits/master"
-        ]
-      ]
+        'title'       => 'Sleepy Mustache',
+        'description' => 'This is the description',
+        'keywords'    => "log, sleepy mustache, framework"
     ]
-  ]);
+);
 
-  $page->show();
+// Content
+$page->bind('header', 'sleepy<span>MUSTACHE</span>');
+$page->bind(
+    'teasers',
+    [
+        [
+            "title"   => "Getting Started",
+            "link"    => 'http://www.sleepymustache.com/',
+            "author"  => "Jaime A. Rodriguez",
+            "date"    => date('m/d/Y', time()),
+            "description" => "
+                Congratulations on successfully installing sleepyMUSTACHE! You can
+                visit the <a
+                href=\"http://www.sleepymustache.com/documentation/index.html\">
+                documentation page</a> to learn more or hit the ground running by
+                viewing the <a
+                href=\"http://www.sleepymustache.com/#getting-started\">getting
+                started</a> section.",
+            "tags"    => [
+                [
+                    'name' => "Configuration",
+                    'link' => "http://www.sleepymustache.com/#getting-started"
+                ]
+            ]
+        ], [
+            "title"   => "Sample Modules",
+            "link"    => "#",
+            "author"  => "Jaime A. Rodriguez",
+            "date"    => date('m/d/Y', time() - 30 * 24 * 60 * 60),
+            "description" => "
+                By default there are 2 sample modules included with the framework.
+                These modules demonstrate how to create your own modules, and
+                implement existing functionality. You may safely delete them.",
+            "tags"    => [
+                [
+                    'name' => "modules",
+                    'link' => "http://www.sleepymustache.com/#default-modules"
+                ], [
+                    'name' => "fixes",
+                    'link' =>
+                        "https://github.com/jaimerod/sleepy-mustache/commits/master"
+                ]
+            ]
+        ]
+    ]
+);
+
+$page->show();
